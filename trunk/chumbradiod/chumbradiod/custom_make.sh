50625dba33b5c80ca74d7927f22436320cb45533
michael.barroco <michael.barroco@93aa27b5-fb84-d659-61d0-171666784eb9>
2011-07-14 07:02:24 +0000

    Modified chumbradiod and chumbhttpd commit

---
+++ trunk/chumbradiod/chumbradiod/custom_make.sh
@@ -0,0 +1,2 @@
+cd src
+arm-linux-g++  -g -O2  -L/home/mc/crad/lib -o chumbradiod  chumbradiod.o crad_interface.o crad_return_codes.o crad_content_handler.o crad_crossdomain_handler.o qndriver.o qnio.o crad_rds_decoder.o -lpthread -lasound /home/mc/crad/lib/libchumbhttpd.a
